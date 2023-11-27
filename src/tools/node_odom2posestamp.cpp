1070b99db0b163d1ab27a3c2194bc1bb2971a02f
omer <omeredemen32@gmail.com>
2023-11-27 12:50:55 +0300

    node_odom2posestamp.cpp -> ros2   - rviz.launch -> ros2

--- src/tools/node_odom2posestamp.cpp
+++ src/tools/node_odom2posestamp.cpp
@@ -1,28 +1,26 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-#include "geometry_msgs/TransformStamped.h"
-#include "geometry_msgs/PoseStamped.h"
-#include "nav_msgs/Odometry.h"
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 #include <fstream>
 
 using namespace  std;
 
-geometry_msgs::PoseStamped latest_pose;
-bool first_msg=1;
-ros::Time lastMsgTime;
-double time_gap=0.1;
+geometry_msgs::msg::PoseStamped latest_pose;
+bool first_msg = true;
+uint64_t lastMsgTime;
+double time_gap = 0.1;
 string odom_topic_in;
 string pose_topic_out;
-int frequency=10;
+int frequency = 10;
 
-ros::Publisher pose_pub;
+rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_pub;
 
 //Call Back Function of motion captrure system
-void callback(const nav_msgs::OdometryConstPtr& msg)
+void callback(const nav_msgs::msg::Odometry::SharedPtr msg)
 {
     if(first_msg)
     {
-        lastMsgTime = msg->header.stamp;
+        lastMsgTime = msg->header.stamp.sec;
         latest_pose.header.frame_id = "world";
         latest_pose.header.stamp=msg->header.stamp;
         latest_pose.pose.orientation.w = msg->pose.pose.orientation.w;
@@ -32,14 +30,14 @@ void callback(const nav_msgs::OdometryConstPtr& msg)
         latest_pose.pose.position.x = msg->pose.pose.position.x;
         latest_pose.pose.position.y = msg->pose.pose.position.y;
         latest_pose.pose.position.z = msg->pose.pose.position.z;
-        pose_pub.publish(latest_pose);
+        pose_pub->publish(latest_pose);
         first_msg = false;
     }
     else
     {
-        if((msg->header.stamp.toSec()-lastMsgTime.toSec())>time_gap)
+        if((msg->header.stamp.sec - lastMsgTime) > time_gap)
         {
-            lastMsgTime = msg->header.stamp;
+            lastMsgTime = msg->header.stamp.sec;
             latest_pose.header.frame_id = "world";
             latest_pose.header.stamp=msg->header.stamp;
             latest_pose.pose.orientation.w = msg->pose.pose.orientation.w;
@@ -49,7 +47,7 @@ void callback(const nav_msgs::OdometryConstPtr& msg)
             latest_pose.pose.position.x = msg->pose.pose.position.x;
             latest_pose.pose.position.y = msg->pose.pose.position.y;
             latest_pose.pose.position.z = msg->pose.pose.position.z;
-            pose_pub.publish(latest_pose);
+            pose_pub->publish(latest_pose);
         }
     }
     return;
@@ -57,16 +55,18 @@ void callback(const nav_msgs::OdometryConstPtr& msg)
 
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "odom2posestamp");
-    ros::NodeHandle n("~");
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<rclcpp::Node>("odom2posestamp");
 
+    node->declare_parameter<std::string>("odom_topic_in");
+    node->declare_parameter<std::string>("pose_topic_out");
+    odom_topic_in = node->get_parameter("odom_topic_in").as_string();
+    pose_topic_out = node->get_parameter("pose_topic_out").as_string();
 
-    n.getParam("odom_in", odom_topic_in);
-    n.getParam("pose_out", pose_topic_out);
+    auto sub = node->create_subscription<nav_msgs::msg::Odometry>(odom_topic_in, 2, callback);
+    pose_pub = node->create_publisher<geometry_msgs::msg::PoseStamped>(pose_topic_out, 2);
 
-    ros::Subscriber sub = n.subscribe(odom_topic_in, 2, callback);
-    pose_pub = n.advertise<geometry_msgs::PoseStamped>(pose_topic_out, 2);
-    ros::spin();
-
+    rclcpp::spin(node);
+    rclcpp::shutdown();
     return 0;
 }
